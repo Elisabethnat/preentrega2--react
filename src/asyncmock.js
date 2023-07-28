a4385c3ce4f1fc8fc66b946a5c62dfd8b9fb68d0
Elisabeth <lelie_angel@hotmail.com>
2023-07-27 22:57:54 -0300

    cambios

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,12 +1,12 @@
 const misProductos = [
-  { id: "1", nombre: "Camisa", precio: 1500, img: "../img/camisa-gaspar.jpg", idCat:"2" },
-  { id: "2", nombre: "Jean", precio: 900, img: "../img/jean-hombre.jpg", idCat:"2" },
-  { id: "3", nombre: "Remera", precio: 500, img: "../img/remeraninos.png", idCat:"2" },
-  { id: "4", nombre: "Zapatillas", precio: 1800, img: "../img/zapatillas.jpg", idCat:"2" },
-  { id: "5", nombre: "Blusa", precio: 600, img: "../img/blusaroja.jpg", idCat:"3" },
-  { id: "6", nombre: "Vestido", precio: 2000, img: "../img/vestido.jpg", idCat:"3" },
-  { id: "7", nombre: "vestidito", precio: 1200, img: "../img/vestido-nina.jpg", idCat:"3" },
-  { id: "8", nombre: "Zapatos", precio: 1300, img: "../img/zapatos-tacon.jpg", idCat:"3" },
+  { id: "1", nombre: "Camisa", precio: 1500, stock:10, img: "../img/camisa-gaspar.jpg", idCat:"2" },
+  { id: "2", nombre: "Jean", precio: 900, stock:10, img: "../img/jean-hombre.jpg", idCat:"2" },
+  { id: "3", nombre: "Remera", precio: 500, stock:10, img: "../img/remeraninos.png", idCat:"2" },
+  { id: "4", nombre: "Zapatillas", precio: 1800, stock:10, img: "../img/zapatillas.jpg", idCat:"2" },
+  { id: "5", nombre: "Blusa", precio: 600, stock:10, img: "../img/blusaroja.jpg", idCat:"3" },
+  { id: "6", nombre: "Vestido", precio: 2000, stock:10, img: "../img/vestido.jpg", idCat:"3" },
+  { id: "7", nombre: "vestidito", precio: 1200, stock:10, img: "../img/vestido-nina.jpg", idCat:"3" },
+  { id: "8", nombre: "Zapatos", precio: 1300, stock:5, img: "../img/zapatos-tacon.jpg", idCat:"3" },
 ]
 
 export const getProductos = () => {
--- src/componentes/CartWidget/CartWidget.jsx
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -1,11 +1,21 @@
+import { useContext } from 'react';
+import { CarritoContext } from '../../context/carritoContext';
+import { Link } from 'react-router-dom';
 import './CartWidget.css'
 
+
 const CartWidget = () => {
+  const {cantidadTotal} =useContext(CarritoContext);
   const imgCarrito = "https://cdn-icons-png.flaticon.com/512/107/107831.png";
   return (
     <div>
-      <img className='imgCarrito' src={imgCarrito} alt="Carrito de compras" />
-      <strong> 10 </strong>
+       <Link to="/cart">
+       <img className='imgCarrito' src={imgCarrito} alt="Carrito de compras" />
+      {
+        cantidadTotal > 0 && <strong> {cantidadTotal} </strong>
+      }
+       </Link>
+      
     </div>
   )
 }
