29da63f4dd5d18eb1d5d4dfe9111adf3d0db1f0f
Elisabeth <lelie_angel@hotmail.com>
2023-07-31 21:14:57 -0300

    ultimos cambios validacion

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -1,114 +1,142 @@
 import { useState, useContext } from "react";
-import { CarritoContext } from "../../context/carritoContext";
+import { CarritoContext } from '../../context/CarritoContext';
 import { db } from "../../services/config";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
 
-const checkout = () => {
+
+const Checkout = () => {
+    const { carrito, vaciarCarrito, total, cantidadTotal } = useContext(CarritoContext);
     const [nombre, setNombre] = useState("");
     const [apellido, setApellido] = useState("");
     const [telefono, setTelefono] = useState("");
     const [email, setEmail] = useState("");
     const [emailConfirmacion, setEmailConfirmacion] = useState("");
     const [error, setError] = useState("");
-    const [ordenId, setOrdenId] = useState("");
+    const [orderId, setOrdenId] = useState("");
 
-    const { carrito, vaciarCarrito, total, cantidadTotal } = useContext(CarritoContext);
+   
 
-    const manejadorFormulario = (event) => {
-        event.preventDeFault();
+    const manejadorFormulario = (e) => {
+        e.preventDefault();
 
-
-        if(!nombre || !apellido || !telefono || !email || !emailConfirmacion){
-            setError("Por favor completa todos los campos.");
+        
+        if (!nombre || !apellido || !telefono || !email || !emailConfirmacion) {
+            setError("Por favor complete todos los campos");
             return;
         }
 
-        if(email !== emailConfirmacion){
-            setError("Los campos del email no coinciden.");
+        
+        if (email !== emailConfirmacion) {
+            setError("Los campos del email no coinciden!!");
             return;
         }
 
+        
         const orden = {
-        items: carrito.map(producto => ({
-            id: producto.item.id, 
-            nombre: producto.item.nombre,
-            cantidad: producto.cantidad
-        })),
-        total: total,
-        fecha: new Date(),
-        nombre, 
-        apellido, 
-        telefono, 
-        email
-    };
+            items: carrito.map(producto => ({
+                id: producto.item.id,
+                nombre: producto.item.nombre,
+                cantidad: producto.cantidad
+            })),
+            total: cantidadTotal,
+            nombre,
+            apellido,
+            telefono,
+            email,
+            precioTotal: total,
+            fecha: new Date()
+        };
 
-    addDoc(collection(db, "ordenes"), orden)
-        .then(docRef =>{
-            setOrdenId(docRef.id);
-            vaciarCarrito();
-        })
-        .catch(error => {
-            console.log("Error al crear la orden", error);
-            setError("Se produjo un error al crear la orden.");
-        })
+         
+
+        Promise.all(
+            orden.items.map(async (productoOrden) => {
+                const productoRef = doc(db, "productos", productoOrden.id);
+                //Por cada producto en la coleecion "productos" obtengo una referencia.
+                const productoDoc = await getDoc(productoRef);
+                const stockActual = productoDoc.data().stock;
+                //Data me permite acceder a la información del documento. 
+
+                await updateDoc(productoRef, {
+                    stock: stockActual - productoOrden.cantidad
+                });
+                
+            })
+        )
+            .then(() => {
+                
+                addDoc(collection(db, "ordenes"), orden)
+                    .then((docRef) => {
+                        setOrdenId(docRef.id);
+                        vaciarCarrito();
+                    })
+                    .catch((error) => {
+                        console.log("Error al crear la orden", error);
+                        setError("Error al crear la orden, vuelva más tarde");
+                    })
+            })
+            .catch((error) => {
+                console.log("Error al actualizar el stock", error);
+                setError("Error al actualizar el stock. Intente nuevamente");
+            })
 
     }
 
-
-
-return(
-    <div>
-        <h2> Checkout </h2>
-        <form onSubmit={manejadorFormulario}>
-            {
-                carrito.map(producto => (
-                    <div key={producto.item.id}>
-                        <p> {producto.item.nombre} x {producto.cantidad}</p>
-                        <p> {producto.item.precio} </p>
+    return (
+        <div>
+            <h2>Checkout</h2>
+            <form onSubmit={manejadorFormulario} className="formulario">
+                {carrito.map(producto => (
+                    <div key={producto.id}>
+                        <p>
+                            {producto.item.nombre} x {producto.cantidad}
+                        </p>
+                        <p>Precio $ {producto.item.precio} </p>
                         <hr />
                     </div>
-                ))
-            }
-            <hr />
-            <div className="form-group">
-                <label htmlfor=""> Nombre </label>
-                <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-            </div>
+                ))}
+                <hr />
 
-            <div className="form-group">
-                <label htmlfor=""> Apellido </label>
-                <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="nombre"> Nombre </label>
+                    <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                </div>
 
-            <div className="form-group">
-                <label htmlfor=""> Telefono </label>
-                <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor=""> Apellido </label>
+                    <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+                </div>
 
-            <div className="form-group">
-                <label htmlfor=""> Email </label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor=""> Telefono </label>
+                    <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                </div>
 
-            <div className="form-group">
-                <label htmlfor=""> EmailConfirmación  </label>
-                <input type="email" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor=""> Email </label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
 
+                <div className="form-group">
+                    <label htmlFor=""> Email Confirmación </label>
+                    <input type="email" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)} />
+                </div>
+
+                {
+                    error && <p style={{ color: "red" }}> {error} </p>
+                }
+
+                <button className="btnComprar" type="submit"> Finalizar Compra </button>
+            </form>
             {
-                error && <p style={{ color: "red" }}> {error} </p>
+                orderId && (
+                    <strong className="ordenId">¡Gracias por tu compra! Tu número de orden es {orderId} </strong>
+                )
             }
 
-            <button type="submit"> Finalizar Compra </button>
-        </form>
-        {
-            ordenId && (
-                <strong> GRacias por tu compra! Tu número de orden es {ordenId}</strong>
-            )
-        }
 
-    </div>
-)
+        </div>
+    )
+}
 
-    }
-    export default checkout
+export default Checkout
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
 import ItemCount from '../ItemCount/ItemCount';
 import { Link } from 'react-router-dom';
-import { carritoContext } from '../../context/carritoContext';
+import { CarritoContext } from '../../context/carritoContext';
 import { useContext } from 'react';
 
-const ItemDetail = ({id, nombre, precio, img, stock}) => {
-   const [agregarCantidad, setAgregarCantidad] = useState(0);
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+  const [agregarCantidad, setAgregarCantidad] = useState(0);
 
-   const { agregarProducto} = useContext(carritoContext);
+  const { agregarProducto } = useContext(CarritoContext);
 
-const manejadorCantidad = (cantidad) => {
-  setAgregarCantidad(cantidad);
-  //console.log("Productos agregados: " + cantidad);
+  const manejadorCantidad = (cantidad) => {
+    setAgregarCantidad(cantidad);
+    //console.log("Productos agregados: " + cantidad);
 
-  const item = {id, nombre, precio};
-  agregarProducto(item, cantidad);
-}
+    const item = { id, nombre, precio };
+    agregarProducto(item, cantidad);
+  }
 
   return (
     <div>
-        <h2>Nombre: {nombre} </h2>
-        <h3>Precio: {precio} </h3>
-        <h3>ID: {id} </h3>
-        <p>Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dictum sem ex, et venenatis lectus suscipit et. Nam sagittis rutrum tincidunt. Vivamus mi diam, iaculis non malesuada vitae, rutrum vel velit. Donec iaculis nibh sapien, sed dignissim urna faucibus at. Mauris risus eros, dapibus consequat ornare non, hendrerit sit amet felis. Vivamus a nisi sit amet metus volutpat efficitur.</p>
-        <img src={img} alt={nombre} />
+      <h2>Nombre: {nombre} </h2>
+      <h3>Precio: {precio} </h3>
+      <h3>ID: {id} </h3>
+      <p>Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dictum sem ex, et venenatis lectus suscipit et. Nam sagittis rutrum tincidunt. Vivamus mi diam, iaculis non malesuada vitae, rutrum vel velit. Donec iaculis nibh sapien, sed dignissim urna faucibus at. Mauris risus eros, dapibus consequat ornare non, hendrerit sit amet felis. Vivamus a nisi sit amet metus volutpat efficitur.</p>
+      <img src={img} alt={nombre} />
 
-        {
-          agregarCantidad > 0 ? (<Link to="/cart"> Terminar Compra </Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/>)
-        }
+      {
+        agregarCantidad > 0 ? (<Link to="/cart"> Terminar Compra </Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
+      }
 
     </div>
   )
